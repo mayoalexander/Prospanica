b144ba118f8ed902c98097ab8063bec77cf1321d
Alexander Mayo <admin@freelabel.net>
2016-07-06 17:17:42 -0500

    initial commit

---
+++ public/homepage-four-grid.php
@@ -0,0 +1,34 @@
+<div class="row">
+	
+	<div class="col-md-3 col-sm-6">
+		<p>
+			<a href="https://www.nshmba.org/?page=Membership" target="_self"><img src="https://www.nshmba.org/resource/resmgr/2014website/Members711.jpg" alt="" height="auto" width="100%"></a>
+		</p>
+		<h3>Membership</h3>
+		<p>Become part of the largest premier Hispanic business organization and take advantage of NSHMBA's wide range of programs and services designed to help grow your personal network, strengthen leadership skills and reach career goals.</p>
+		<br>
+		<p><a href="https://www.nshmba.org/?page=Membership" target="_self">More Information</a></p>
+	</div>
+	<div class="col-md-3 col-sm-6">
+		<p><a href="https://www.nshmba.org/?page=PartnersSponsors"><img src="https://www.nshmba.org/resource/resmgr/2014website/Partners711.jpg" alt="" height="auto" width="100%"></a></p>
+		<h3>Partners</h3>
+		<p>Become a NSHMBA partner and discover the best Hispanic talent. Counting with over 25,000 members and 39 chapters, we offer programs and products throughout the year that allow you to tell your brand story to the niche audience of Hispanic business professionals. </p>
+		<br>
+		<p><a href="https://www.nshmba.org/?page=PartnersSponsors" target="_self">More Information</a></p>
+	</div>
+	<div class="col-md-3 col-sm-6">
+		<p><a href="http://www.nektpro.com/" target="_blank"><img src="https://www.nshmba.org/resource/resmgr/2014website/Jobops711.jpg" alt="" height="auto" width="100%"></a></p>
+		<h3>Job Opportunities</h3>
+		<p>Take the next step towards success and join the largest premier Hispanic job site network. A place where members are connected to partners who are looking for high performance talent. Connect with leading companies and make the right professional connections!</p>
+		<br>
+		<p><a href="http://www.nektpro.com/" target="_blank">More Information</a></p>
+	</div>
+	<div class="col-md-3 col-sm-6">
+		<p><a href="https://www.nshmba.org/?page=Scholarship" target="_blank"><img src="https://www.nshmba.org/resource/resmgr/2014website/Scholors711.jpg" alt="" height="auto" width="100%"></a></p>
+		<h3>Foundation</h3>
+		<p>Apply today and discover the perfect scholarships and awards  designed just for you!</p>
+		<br>
+		<p><a href="https://www.nshmba.org/donation" target="_blank">More Information</a></p>
+	</div>
+
+</div>
