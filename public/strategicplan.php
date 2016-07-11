2cba3ec9d91adb9919c87470f3f9b5e4c20642ba
Alexander Mayo <admin@freelabel.net>
2016-07-10 19:42:33 -0500

    fixed front page and secondary pages

---
+++ public/strategicplan.php
@@ -0,0 +1,59 @@
+<section class="row">
+	<div class='col-md-6'>
+		<iframe src="https://www.youtube.com/embed/7lT3hPHnABI" allowfullscreen="" frameborder="0" class="video_player" height="275px"></iframe>
+	</div>
+
+	<div class='col-md-6'>
+		<p>The National Society of Hispanic MBAs (NSHMBA) commenced in 1988 with a vision to be THE premier Hispanic business organization for Hispanic business professionals. Dedicated to increasing the number of Hispanics graduating with MBA’s, NSHMBA opens doors for Hispanic talent by sourcing leadership positions, securing job placement and enhancing professional development.</p>
+		<!-- <p>Current trends depict that Hispanics are the largest and fastest growing minority group, having the highest drop-out rate, and are severely underrepresented in professional employment. With Hispanics expected to account for 60 percent of the U.S. populations growth between 2005 and 2050, NSHMBA knows that Hispanic success in education and the labor market is of immediate and long-term importance to the nation’s economy.</p> -->
+	</div>
+</section>
+
+<section class="row">
+	<div class='col-md-6'>
+		<p>Though Hispanics with MBAs had been the sole focus, 2015 marks the year in which NSHMBA extends its reach beyond the MBA community to undergraduate and high school-level students. NSHMBA realizes by building Hispanic talent from the ground up, they are empowering Hispanic business professionals to achieve their full educational, economic and social potential.</p>
+	</div>
+
+	<div class='col-md-6'>
+		<iframe src="https://onedrive.live.com/embed?cid=E38720130CB14779&resid=E38720130CB14779%213728&authkey=ACZVsxqKTEhnxbs&em=2" scrolling="no" frameborder="0" height="327" width="402"></iframe>
+	</div>
+</section>
+
+<h2>NSHMBA 2015 Strategy & Beyond</h2>
+<section class="row about_us">
+	<div class="col-md-6">
+		<i class="fa fa-eye"></i>
+		<h4>Vision</h4>
+		<p>Be the leading catalyst for Hispanic achievement by 2025</p>
+	</div>
+
+	<div class="col-md-6">
+		<i class="fa fa-rocket"></i>
+		<h4>Our Mission</h4>
+		<p>Empower Hispanic business professionals to achieve their full educational, economic and social potential</p>
+	</div>
+
+	<div class="col-md-6">
+		<i class="fa fa-rocket"></i>
+		<h4>Vivid Description</h4>
+		<p>Hispanics will share in the affluence of the United States in proportion to their share of the population, as they attain higher education and leadership positions in record numbers. The entire country will be stronger as a result of greater participation from the Hispanic community.</p>
+	</div>
+
+	<div class="col-md-6">
+		<i class="fa fa-rocket"></i>
+		<h4>Major Goals</h4>
+		<ul>
+			<li>Increased Hispanic Participation in formal education</li>
+			<li>Accelerate Hispanic advancement through entrepenuership and corperate placement</li>
+			<li>Become a leading, nationally recognized and respected Hispanic professional organization</li>
+		</ul>
+	</div>
+</section>
+
+<p>Read More <a href="http://sponsorslogo.nshmba.org/DownloadFile.aspx?File=nt9BQpbAHu7yVC5caYq1XI9BAs0OsRE5BjX8T/pGe1eyEfJxfKg+ntbFLtKQ5bWDSEvIPBUqDI6kBrVvRvO/Hg==">HERE</a></strong> on the full press release.</p>
+
+<p><span style="font-weight: bold;">Phone number:</span> 214.596.9338<br>
+<br>
+<span style="font-weight: bold;">Toll Free:</span> 877.467.4622<br>
+<br>
+<span style="font-weight: bold;">Fax:</span> 214.596.9325</p>
